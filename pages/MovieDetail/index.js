9e11a2022d11e56300cc975dfa7e61cffab96501
2624431797 <2624431797@qq.com>
2020-04-03 17:50:30 +0800

    MovieDetail

--- pages/MovieDetail/index.js
+++ pages/MovieDetail/index.js
@@ -3,6 +3,8 @@ import { requestPost } from "../../services/index"
 Page({
   data: {
     movielist: [],
+    animate: "",
+    animateFlag: true
   },
   //获取数据
   handlerInitMovieList(id){
@@ -25,6 +27,26 @@ Page({
           timingFunc: 'easeInOut'
         }
       })
+    })
+  },
+  //动画
+  handlerTogger(){
+    let animation = wx.createAnimation({
+      duration: 500,
+      transformOrigin: '50% 50%',
+      timingFunction: 'linear'
+    })
+    let {animateFlag} = this.data
+    if(animateFlag){
+      animation.translateY(-200).step()
+      this.setData({animateFlag: false})
+    }
+    else{
+      animation.translateY(0).step()
+      this.setData({animateFlag: true})
+    }
+    this.setData({
+      animate: animation.export()
     })
   },
   handlerRealm(){
