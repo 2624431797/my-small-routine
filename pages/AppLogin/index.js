500f919e6fb3a70136965941238c9c2f401b5f61
2624431797 <2624431797@qq.com>
2020-04-17 16:21:24 +0800

    AppLogin

--- pages/AppLogin/index.js
+++ pages/AppLogin/index.js
@@ -1,53 +1,71 @@
 Page({
   data: {
-    username: "",
-    password: "",
-    sms: "",
+    //判断小程序的API，回调，参数，组件等是否在当前版本可用
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isAuthorization: false,   //登录状态
     isLoading: false
   },
-  //表单提交
-  handlerGetUserName(e){
-    this.setData({username: e.detail})
+  //初始化
+  onLoad(options) {
+    this.handlerBindGetOnLoad()
   },
-  handlerGetPassWord(e){
-    this.setData({password: e.detail})
+  //初始化判断
+  handlerBindGetOnLoad() {
+    // 查看是否授权
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: res => {
+              //从数据库获取用户信息
+              //console.log(res)
+              //修改验证
+              this.setData({ 
+                isAuthorization: true
+              })
+              wx.setStorageSync("isAuthorization", this.data.isAuthorization)
+              //用户已经授权过
+              wx.switchTab({
+                url: "/pages/index/index"
+              })
+            }
+          })
+        }
+      }
+    })
   },
-  handlerFormSubmit(){
-    this.setData({isLoading: true})
-    const {username} = this.data
-    const {password} = this.data
-    const {sms} = this.data
-    if(!username){
-      this.setData({isLoading: false})
-      wx.showModal({
-        content: '请输入手机号',
-        showCancel: false
+  handlerBindGetUserInfo(e) {
+    //用户按了允许授权按钮
+    if (e.detail.userInfo) {
+      // 通过 wx.login() 完成微信登录
+      // wx.login()
+      // 通过 wx.getUserInfo() 获取用户信息
+      wx.getUserInfo({
+        lang: "zh_CN",
+        success: function(res) {
+          let {userInfo} = res
+          wx.setStorageSync("AppUserInfo", userInfo)
+          wx.setStorageSync("AppCity", userInfo.city)
+        }
       })
-      return false
-    }
-    else if(!password){
-      this.setData({isLoading: false})
-      wx.showModal({
-        content: '请输入密码',
-        showCancel: false
+      //登录状态
+      this.setData({ 
+        isAuthorization: true
       })
-      return false
-    }
-    else if(!sms){
-      this.setData({isLoading: false})
-      wx.showModal({
-        content: '请输入验证码',
-        showCancel: false
+      wx.setStorageSync("isAuthorization", this.data.isAuthorization)
+      //授权成功后，跳转进入小程序首页
+      wx.switchTab({
+        url: "/pages/index/index"
       })
-      return false
+    } 
+    else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '警告',
+        content: '您选择拒绝授权, 将无法进入小程序, 请授权之后再进入',
+        showCancel: false,
+        confirmText: "返回授权"
+      })
     }
-    else{
-      setTimeout(() => {
-        this.setData({isLoading: false})
-      }, 2000)
-    }
-  },
-  onLoad(options){
-    
   }
 })
--- pages/Mine/index.js
+++ pages/Mine/index.js
@@ -1,8 +1,16 @@
 Page({
   data: {
-
+    isAuthorization: false,
+    AppUserInfo: null
   },
+  //初始化
   onLoad(options){
-
+    this.handlerGetUserInfo()
+  },
+  handlerGetUserInfo(){
+    this.setData({
+      isAuthorization: wx.getStorageSync("isAuthorization"),
+      AppUserInfo: wx.getStorageSync("AppUserInfo")
+    })
   }
 })
