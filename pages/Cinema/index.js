f8e25ed20d72771380af41cd1bb7d0390dabfb50
2624431797 <2624431797@qq.com>
2020-05-09 17:50:17 +0800

    map

--- pages/Cinema/index.js
+++ pages/Cinema/index.js
@@ -1,66 +1,34 @@
-// pages/Cinema/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    noticeText: "受疫情影响，各大影院特推出优惠券百分百中活动，体验浪漫情怀，享受视听盛宴；体验时尚生活，品味精彩电影；感受影音魅力，乐享震撼视听；新视界，新天地，心享受！",
+    nowCity: "",
+    vanActive: 0,
+    cinemalist: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    let para = "1"
+    this.handlerInitGetList(para)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //获取城市
+  onShow(){
+    let nowCity = wx.getStorageSync("AppCity")
+    this.setData({
+      nowCity
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //获取数据
+  handlerInitGetList(para){
+    let params = {para}
+    this.requestPost(
+      `/api/c1/GetCineListChange`, params
+    ).then(res => {
+      this.setData({
+        cinemalist: res.data.cinemalist
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //切换
+  handlerVanOnChange(event){
+    //this.handlerInitGetList(event.detail.name)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
---
+++ pages/CinemaDetail/index.js
@@ -0,0 +1,81 @@
+Page({
+  data: {
+    showMap: false,
+    userlist: null,
+    longitude: 0,
+    latitude: 0,
+    //标记点用于在地图上显示标记的位置
+    markers: [],
+    //指定一系列坐标点，从数组第一项连线至最后一项
+    polyline: []
+  },
+  onLoad(options) {
+    this.handlerInitGetData(options.id)
+  },
+  handlerBack() {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  handlerInitGetData(id) {
+    let parms = { id }
+    this.requestPost(
+      `/api/c1/GetCineDetail/${id}`, parms
+    ).then(res => {
+      this.setData({
+        userlist: res.data,
+        showMap: true,
+      })
+      let {iLong, iLat} = res.data
+      this.handlerInitMap(iLong, iLat)
+    })
+  },
+  handlerInitMap(iLong, iLat) {
+    //获取当前位置
+    wx.getLocation({
+      type: "wgs84",
+      success: res => {
+        let {longitude, latitude} = res
+        //markers
+        let markers = [{
+          id: 0,
+          iconPath: "../../assets/maps/map-position.png",
+          width: 24,
+          height: 24,
+          longitude: iLong,
+          latitude: iLat,
+          callout: {
+            display: "BYCLICK",
+            content: "影院位置",
+            color: "#1296db",
+            borderRadius: 5,
+            padding: 6,
+            textAlign: "center",
+            borderColor: "#CCC",
+          }
+        }]
+        //polyline
+        let polyline = [{
+          points: [{
+            longitude,
+            latitude
+          }, {
+            longitude: iLong,
+            latitude: iLat
+          }],
+          color: "#FF0000DD",
+          width: 2,
+          dottedLine: true
+        }]
+        //初始化数据
+        this.setData({
+          markers, polyline, latitude, longitude
+        })
+      }
+    })
+  },
+  //点击标记点时触发
+  markertap(e) {
+    console.log(e.detail.markerId)
+  }
+})
